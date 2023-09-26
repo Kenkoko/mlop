25e1b5cd927af6839793067c409ce977be644d08
Dung Huynh <dung.huynh@kongsbergdigital.com>
2023-09-26 20:36:16 +0200

    extract coef and intercept from linear model

--- training/trainers.py
+++ training/trainers.py
@@ -6,6 +6,34 @@ def train_linear_regression(X_train, y_train):
     # make sure we have pandas for this step, we need it to use the data_frame
     import pandas as pd  # noqa
     from sklearn.linear_model import LinearRegression
+    from clearml import Logger
+    import training
+
     model = LinearRegression()
     model.fit(X_train, y_train)
+
+    Logger.current_logger().report_table(
+        title='Regression Report',
+        series='Coefficient and Interception',
+        table_plot = training.extract_coef_intercept(model)
+    )
+
     return model
+
+
+def extract_coef_intercept(model):
+    # Extract coefficients and interception from the SciKit Linear Reg model
+    # input
+    #   @param model: Scikit Model
+    # output:
+    #   Pandas table
+    
+    import pandas as pd
+
+    output = {
+        "const": model.intercept_
+    }
+    for idx, coef in enumerate(model.coef_) :
+        output[f'x{idx + 1}'] = round(coef, 4)
+    
+    return pd.DataFrame(output, index=[0])
